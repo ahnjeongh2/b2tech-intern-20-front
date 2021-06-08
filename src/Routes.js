a6d71e3058b4a23042612179c117c6a84fda29bf
Yurim Kim <YR.KIM@MacBook-Air.local>
2021-06-08 18:29:29 +0900

    초기 셋팅

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GlobalStyles from '../src/styles/GlobalStyles';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <GlobalStyles />
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
