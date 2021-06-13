c5c602196e82c32fa277af01f2cf50d7a6584e5e
user <user@userui-MacBookPro.local>
2021-06-13 22:06:21 +0900

    Add: 개인별 마이페이지(1차) 구현   - 휴가/근무제 신청 버튼 컴포넌트 활용

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 // import Permissions from './pages/Main/Permissions';
+import MyPage from './pages/MyPage/MyPage';
+import Request from './pages/Main/Request/Request';
 
 class Routes extends React.Component {
   render() {
@@ -10,6 +12,8 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Main} />
           {/* <Route exact path="/permissions" component={Permissions} /> */}
+          <Route exact path="/mypage" component={MyPage} />
+          <Route exact path="/request" component={Request} />
         </Switch>
       </Router>
     );
